085a1cf22ebb06fe70dcb2dbf6598df7af52d81a
hvf <hvf@robotarmymade.(none)>
2010-10-06 17:24:39 +0000

    cleanup deploy - period host working

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,16 @@
 ###
-default_run_options[:pty] = true
 set :use_sudo, false
 set :application, "logstew"
 set :deploy_to, "/home/hvf/deploy/#{application}"
 set :repository,  "git://github.com/robotarmy/logstew.git"
 set :scm, :git
 set :branch, 'master'
-set :scm_verbose, true
 set :deploy_via, :remote_cache
 ssh_options[:forward_agent] = true
 
-role :web, "proxy-logstew.robotarmymade.com"                          # Your HTTP server, Apache/etc
-role :app, "app-logstew.robotarmymade.com"                          # This may be the same as your `Web` server
-role :db,  "master-db-logstew.robotarmymade.com", :primary => true # This is where Rails migrations will run
+role :web, "proxy.logstew.robotarmymade.com"                          # Your HTTP server, Apache/etc
+role :app, "app.logstew.robotarmymade.com"                          # This may be the same as your `Web` server
+role :db,  "master.db.logstew.robotarmymade.com", :primary => true # This is where Rails migrations will run
 #role :db,  ""
 
 # If you are using Passenger mod_rails uncomment this:
