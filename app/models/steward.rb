5703bed2b661d2cfeb6f90d1343261fdef64a46c
Curtis J Schofield <178963+robotarmy@users.noreply.github.com>
2010-10-10 03:29:25 -0400

    ino confirmablew  wrecks the happy path

--- app/models/steward.rb
+++ app/models/steward.rb
@@ -7,5 +7,5 @@ class Steward
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
-	 :recoverable, :rememberable, :trackable, :validatable, :confirmable
+	 :recoverable, :rememberable, :trackable, :validatable
 end
