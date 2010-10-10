199204002617de41de1e834c0925119868a4b357
Curtis J Schofield <178963+robotarmy@users.noreply.github.com>
2010-10-10 03:16:43 -0400

    happy path complete
    address set on signup after user created
    
    redirects to logs_path instead of welcome if signed in or remembered
    
    forgotten password link on welcome page
    w

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   def after_sign_in_path_for(resource)
-    if resource.is_a?(Steward) 
-	new_log_url
+    if resource.is_a?(Steward)
+	if resource.address.blank?
+		new_address_path
+	else
+		new_log_path
+	end
     else
       super
     end
--- app/controllers/stewards_controller.rb
+++ app/controllers/stewards_controller.rb
@@ -1,2 +1,3 @@
 class StewardsController < ApplicationController
+
 end
