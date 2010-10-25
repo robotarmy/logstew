b0e63706a6c446f6e3540071b03da7c8f374a2bc
Curtis J Schofield <github.com@robotarmyma.de>
2010-10-24 17:04:28 -0700

    added starboard logs - opt in and changed copy for your logs to my stewardship

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def render_last_posts
+    out = ""
+    Steward.desc(:last_sign_in_at).limit(10).each do |n|
+      log =  n.logs.desc(:updated_at).first
+      if log
+        out << render({:partial => 'logs/log', :object => log}).html_safe
+      end
+    end
+    out.html_safe
+  end
 end
