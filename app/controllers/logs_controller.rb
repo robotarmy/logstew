aa699caef35ffaef8981aa3d531062a9a451d721
Curtis J Schofield <178963+robotarmy@users.noreply.github.com>
2010-11-11 09:33:47 -0800

    added edit, edit link, unable to edit others, images, no validations+edit == win

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -9,18 +9,14 @@ class LogsController < ApplicationController
     end
   end
 
-  # GET /logs/new
-  # GET /logs/new.xml
   def new
     @log = current_steward.logs.new
-
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @log }
     end
   end
-  # GET /logs/:id
-  # GET /logs/:id.xml
+
   def show
     Steward.criteria.id(params[:steward_id]).each do  |a|
       @log = a.logs.find(params[:id])
@@ -30,8 +26,37 @@ class LogsController < ApplicationController
       format.xml  { render :xml => @log }
     end
   end
-  # POST /logs
-  # POST /logs.xml
+
+  def edit
+    # This is neat - as there is an implied security check
+    # as the edit link is encoded with the current_steward 
+    # and the specific log id
+    Steward.criteria.id(params[:steward_id]).each do  |a|
+      @log = a.logs.find(params[:id])
+    end
+    if @log  # not found
+      respond_to do |format|
+        format.html
+        format.xml  { render :xml => @log }
+      end
+    else 
+      redirect_to(welcome_index_path)
+    end
+  end
+
+  def update
+    Steward.criteria.id(params[:steward_id]).each do  |a|
+      @log = a.logs.find(params[:id])
+    end
+    respond_to do |format|
+      if @log.update_attributes(params[:log])
+        format.html { redirect_to(logs_path, :notice => 'Log was successfully created.') }
+      else
+        format.html { render :action => "edit" ,:notice => 'Nope'}
+      end
+    end
+  end 
+
   def create
     @log = current_steward.logs.build(params[:log])
     respond_to do |format|
