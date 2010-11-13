9baee06964622a685afd3db3cba66a4db7b39a6e
Curtis J Schofield <178963+robotarmy@users.noreply.github.com>
2010-11-12 21:16:40 -0800

    style

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,12 +5,32 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :s3
-   
+  version :seventwenty do
+    process :resize_to_fill => [720,424]
+  end
+
+  version :sixfourty do
+    process :resize_to_fill => [640,480]
+  end
+
+  version :twelveeighty do
+    process :resize_to_fill => [1280,753]
+  end
+
   version :fourfifty do
     process :resize_to_fill => [450,253]
   end
+
+  version :threetwenty do
+    process :resize_to_fill => [320,240]
+  end
+
   version :thumb do
-    process :resize_to_fill => [160,90]
+    process :resize_to_fill => [201,118]
+  end
+
+  version :mini_square do
+    process :resize_to_fill => [32,32]
   end
   version :mini_thumb do
     process :resize_to_fill => [16,9]
