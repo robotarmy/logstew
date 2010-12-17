35d6f5376fdbb58682c285e4c7d67ff50b870ee5
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2010-12-16 20:21:49 -0800

    ops routing

--- spec/controllers/ops_controller.rb
+++ spec/controllers/ops_controller.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe OpsController do
+  describe "routes" do
+    it "should properly route" do
+      {:get, "/op/host"}.should route_to(:controller => "ops", :action => "host")
+      {:get, "/op/git"}.should route_to(:controller => "ops", :action => "git")
+    end
+  end
   it "has #host" do
     get :host 
     assert {
