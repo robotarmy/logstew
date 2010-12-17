1751496f835fa8f8e049495d58895b7308aa3577
Curtis Schofield <github@robotarmyma.de>
2010-12-16 20:16:33 -0800

    route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Logstew::Application.routes.draw do
     resources :logs 
     resources :addresses , :only => [:new,:create]
   end
+  resource :op, :only =>[] do
+    get :host
+    get :git
+  end
   get "welcome/index"
   root :to => "welcome#index"
   match "/images/uploads/*path" => "grid_fs#serve"
