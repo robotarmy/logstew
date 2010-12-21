7912308ceedaf1053616066404b96193d0b5019a
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2010-12-21 12:27:32 -0800

    no answers

--- app/models/ditto.rb
+++
@@ -1,7 +0,0 @@
-class Ditto
-  include Mongoid::Document
-  field :by 
-  field :for
-  field :what 
-  validates_presence_of :what,:for,:by
-end
--- app/models/question.rb
+++
@@ -1,7 +0,0 @@
-class Question
-  include Mongoid::Document
-  field :body
-  embeds_many :answers
-  referenced_in :log
-  validates_presence_of :body
-end
