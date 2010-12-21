4b933b7ff081e9aec39ebc1f6548090ccafd75bc
Curtis Schofield <github@robotarmyma.de>
2010-12-21 13:25:47 -0800

    helper

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -2,4 +2,8 @@ module LogsHelper
   def logs_title
     "my stewardship journal"
   end
+  def disqus_for(log)
+    render :partial =>'logs/helper/disqus_for_log', 
+      :locals => {:log => log}
+  end
 end
