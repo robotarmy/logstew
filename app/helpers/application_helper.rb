ad4aa257b47f78af3dcc5c703db066506822d01b
Curtis Schofield <github@robotarmyma.de>
2010-12-21 16:30:34 -0800

    steward

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
     Steward.desc(:last_sign_in_at).limit(10).each do |n|
       log =  n.logs.desc(:updated_at).first
       if log
-        out << link_to(steward.name,steward_logs_path(log.steward)).html_safe
+        out << link_to(log.steward.name,steward_logs_path(log.steward)).html_safe
         out << render({:partial => 'logs/log', :object => log}).html_safe
       end
     end
