e9177719a573475d1a344dcc139ece49747e5cfa
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2010-12-21 16:19:49 -0800

    app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,8 @@ module ApplicationHelper
     Steward.desc(:last_sign_in_at).limit(10).each do |n|
       log =  n.logs.desc(:updated_at).first
       if log
-        out << link_to(log.steward.address.public_name,steward_logs_path(log.steward)).html_safe
+        name = log.steward.address.public_name || log.steward.address.name || "Anonymous"
+        out << link_to(name,steward_logs_path(log.steward)).html_safe
         out << render({:partial => 'logs/log', :object => log}).html_safe
       end
     end
