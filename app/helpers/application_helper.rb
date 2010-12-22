e7b58cdaf30c1da95c9ae36f812e14e075e93cbb
Curtis Schofield <github@robotarmyma.de>
2010-12-21 16:24:49 -0800

    sensible refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,7 @@ module ApplicationHelper
     Steward.desc(:last_sign_in_at).limit(10).each do |n|
       log =  n.logs.desc(:updated_at).first
       if log
-        name = log.steward.address.public_name || log.steward.address.name || "Anonymous"
-        out << link_to(name,steward_logs_path(log.steward)).html_safe
+        out << link_to(steward.name,steward_logs_path(log.steward)).html_safe
         out << render({:partial => 'logs/log', :object => log}).html_safe
       end
     end
