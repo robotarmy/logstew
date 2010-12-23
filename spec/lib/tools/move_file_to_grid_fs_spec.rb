deeb293d8200b5e6190de593f52266af21fd201a
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2010-12-22 21:41:54 -0800

    rake task - better testing - objectid support printout

--- spec/lib/tools/move_file_to_grid_fs_spec.rb
+++ spec/lib/tools/move_file_to_grid_fs_spec.rb
@@ -2,27 +2,68 @@ require 'spec_helper'
 require 'fileutils'
 
 describe MoveFileToGridFs do
-  
+
+  context "a steward doesn't exist for the log file" do
+
+    let(:file_path) do
+      "log/image/cake/fish.png"
+    end
+
+    let(:mtg) do
+      MoveFileToGridFs.new(file_path)
+    end
+
+    it "sets log to string error message " do
+      assert {
+        mtg.log =~ /illegal/
+      }
+    end
+    it "raises error if cannot update" do
+      assert {
+        rescuing {
+          mtg.update(:grid)
+        }
+      }
+    end
+    it "returns false if doesn't exist" do
+      assert {
+        mtg.exists_as?(:grid) == false
+      }
+    end
+  end
+
   context "a steward has a log with a on disk file" do
+    after(:all) do
+      dir_path = File.join(Rails.root,
+                           'spec',
+                           'data',
+                           'move_file_to_grid_fs_spec',
+                           'log')
+      FileUtils.rm_rf dir_path
+    end
+
     let(:steward) do
       Factory(:steward)
     end
 
     let(:log) do
-      steward.logs.create(:_id => 'move_file_to_grid_fs_spec_id',
-                          :title => 'move_file_to_grid_fs_spec',
+      steward.logs.create(:title => 'move_file_to_grid_fs_spec',
                           :image_filename => 'rails.png')
     end
 
     let(:file_path) do
-      file_path = File.join(Rails.root,
-                            'spec',
-                            'data',
-                            'move_file_to_grid_fs_spec',
-                            'log',
-                            'image',
-                            log.id,
-                            log.image_filename)
+      dir_path = File.join(Rails.root,
+                           'spec',
+                           'data',
+                           'move_file_to_grid_fs_spec')
+      log_image_path = File.join(dir_path,
+                                 'log',
+                                 'image',
+                                 log.id.to_s)
+      FileUtils.mkdir_p log_image_path
+      FileUtils.copy(File.join(dir_path,log.image_filename),
+                     log_image_path)
+      File.join(log_image_path,log.image_filename)
     end
 
     let(:mtg) do
@@ -37,7 +78,7 @@ describe MoveFileToGridFs do
 
     it " identifies the log vi the file path " do
       assert  {
-        mtg.log_id == log.id
+        mtg.log_id == log.id.to_s
       }
     end
     
@@ -52,8 +93,8 @@ describe MoveFileToGridFs do
       assert {
         mtg.exists_as?(:grid)
       }
+
     end
-    
   end
 end
 
