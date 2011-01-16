f18ac7604270998197654dacdeb145fc8e56e21d
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2011-01-16 15:48:39 -0800

    file storage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
-  config.grid_fs_database = Mongoid.primary.name
-  config.grid_fs_host = Mongoid.primary.connection.primary.first
-  config.storage = :grid_fs
-  config.grid_fs_access_url = "/images"
+  #config.grid_fs_database = Mongoid.primary.name
+  #config.grid_fs_host = Mongoid.primary.connection.primary.first
+  #config.storage = :grid_fs
+  #config.grid_fs_access_url = "/images"
 end
 
 
