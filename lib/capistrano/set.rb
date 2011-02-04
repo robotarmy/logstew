4d171fae8cd6976e374d6257ac26c2f9ab41f3b2
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2011-02-03 16:46:58 -0800

    submodule

---
+++ lib/capistrano/set.rb
@@ -0,0 +1,21 @@
+Capistrano::Configuration.instance(:must_exist).load do
+  set :whenever_command, "bundle exec whenever"
+  set :rvm_type, :system
+  set :rvm_ruby_string, 'ruby-1.9.2' 
+  set :user , :hvf.to_s
+  set :use_sudo, false
+  set :application, "logstew"
+  set :deploy_to, "/home/hvf/deploy/#{application}"
+  set :repository,  "git://github.com/robotarmy/logstew.git"
+  set :scm, :git
+  set :branch, 'master'
+
+  set :git_enable_submodules, 1
+
+  set :deploy_via, :remote_cache
+
+  ssh_options[:forward_agent] = true
+
+  role :app, "md1.robotarmymade.com"
+  role :db,  "md1.robotarmymade.com"
+end
