aad7d856623b9108e5082aabce3b18a96bc3e575
Curtis Schofield <github@robotarmyma.de>
2011-02-11 20:03:18 -0800

    wheneve

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,3 @@
-#require "whenever/capistrano"
-require 'bundler/capistrano'
-
 Dir[File.join(File.dirname(__FILE__),"../lib/capistrano/*.rb")].each {|f| require f}
+require 'bundler/capistrano'
+require "whenever/capistrano"
