c6a0c183a623d15d96fd4791af315da54c819f62
Curtis Schofield <github.com@robotarmyma.de>
2011-02-26 22:15:18 -0800

    new specs

--- spec/integration/log_index_spec.rb
+++ spec/integration/log_index_spec.rb
@@ -5,18 +5,20 @@ describe Log do
     s.create_address(:public_name => :sam.to_s)
     s
   end
+
   let(:log) do
     steward.logs.create(:story => 'storytime')
   end
+
   context "log in" do
     before(:each) do
-      visit(steward_session_path)
-      fill_in 'steward[email]', :with => steward.email
-      fill_in 'steward[password]', :with => 'histewry'
-      click_button 'enter'
+      sign_in steward
     end
+
     context "index page" do
       it "has more entries by the same creator" do
+        pending
+
       end
     end
   end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,28 +1,24 @@
 require 'spec_helper'
 
 describe Address do
-  it "should have a zipcode" do
-    stew = 	Steward.create(:email => 'e@email.com',:password => 'pass')
+  let(:stew) do
+    stew = Factory(:steward)
     stew.build_address(:public_name => 'Curtis', :zipcode => '94102',:public_location_name => 'Hayes Valley Farm')
+    stew
+  end
+  it "should have a zipcode" do
     assert {
       stew.address.zipcode == '94102'
-      
     }
   end
   it "should have a public_location_name" do
-    stew = 	Steward.create(:email => 'e@email.com',:password => 'pass')
-    stew.build_address(:public_name => 'Curtis', :zipcode => '94102',:public_location_name => 'Hayes Valley Farm')
     assert {
       stew.address.public_location_name == 'Hayes Valley Farm'
-      
     }
   end
   it "should have a public_name" do
-    stew = 	Steward.create(:email => 'e@email.com',:password => 'pass')
-    stew.build_address(:public_name => 'Curtis', :zipcode => '94102',:public_location_name => 'Hayes Valley Farm')
     assert {
-      stew.address.public_name == 'Curtis'
-      
+      stew.address.public_name
     }
   end
 end
