28a695c8009a50dbcfafbd7c35f7a55a4eef2ed5
Curtis Schofield <178963+robotarmy@users.noreply.github.com>
2011-03-16 22:21:30 -0700

    still need to pull out get_recent_logs into a model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,30 @@
 module ApplicationHelper
 
-  def render_last_posts
+  def get_recent_logs
     out = []
-    Steward.descending(:last_sign_in_at).limit(10).each do |n|
-      log =  n.logs.desc(:updated_at).first
+    Steward.descending(:last_sign_in_at).limit(10).each do |steward|
+      log =  steward.logs.desc(:updated_at).first
+      out << log
+    end
+    out
+  end
+
+  def capture_recent_logs
+    out = []
+     get_recent_logs.each do |log|
       if log
         out << capture do #html_safe
           div_for log do
             link_to(log.creator_name,steward_logs_path(log.steward))
-            render({:partial => 'logs/log', :object => log})
+            render log
           end
         end
       end
     end
-    out.join.html_safe
+    out
+  end
+
+  def render_last_posts
+    capture_recent_logs.join.html_safe
   end
 end
