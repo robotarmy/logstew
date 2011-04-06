0972dc044ce1f32af60747eebde36fd0b00edebb
Curtis + Pamela <git+c+p@ram9.cc>
2011-04-05 17:49:37 -0700

    merge log helper changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,15 +4,24 @@ module ApplicationHelper
     steward && steward == current_steward
   end
 
+
+  def render_last_posts
+    capture_recent_logs.join.html_safe
+  end
+
   def get_recent_logs
     out = []
     Steward.descending(:last_sign_in_at).limit(10).each do |steward|
-      log =  steward.logs.desc(:updated_at).first
-      out << log
+      log = steward.logs.all.or(
+        :image_filename.exists => true, 
+        :image_filename.ne => '').or(
+        {:title.exists => true, 
+          :title.ne => ''}).first
+
+      out << log if log
     end
     out
   end
-
   def capture_recent_logs
     out = []
      get_recent_logs.each do |log|
@@ -20,15 +29,16 @@ module ApplicationHelper
         out << capture do #html_safe
           div_for log do
             link_to(log.creator_name,steward_logs_path(log.steward))
+            # 
+            # why doesn't
+            #  'render log' 
+            # work?
+            #
             render({:partial => 'logs/log', :object => log})
           end
         end
       end
     end
     out
-  end
-
-  def render_last_posts
-    capture_recent_logs.join.html_safe
   end
 end
